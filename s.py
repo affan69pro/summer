2b3b6b52f90778aa819aab51fa2d5aa1deae714e
affan50pro <sweet.baby12325@gmail.com>
2024-12-09 10:32:47 +0400

    first commit

---
+++ s.py
@@ -0,0 +1,7 @@
+temp=float(input("enter the temperature in your country: "))
+if temp>35:
+    print(temp,"℃ Hmm...you should wear a T-shirt👕")
+elif 35>temp>25:
+    print(temp,"℃ Hmm...you should wear a full sleeve shirt🥼")
+elif 25>temp<10:
+    print(temp,"℃ Hmm...you should wear a jacket🧥")
